31c9e97a1af35d09a627a4acbcf7d8c831a38044
M0n00s <programacion.buffalo@gmail.com>
2024-02-05 14:08:25 -0400

    add maps componet

--- src/component/maps/Maps.jsx
+++ src/component/maps/Maps.jsx
@@ -1,12 +1,32 @@
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
 import "./maps.css";
+import { Markers } from "./Markers";
 
 export const Maps = () => {
+  const position = { lat: 25.77427, lng: -80.19366 };
+
   return (
     <section id="location" className="mapsCont">
       <h3>
         Find Your Local <span className="textNaranja">Prosigma</span>
       </h3>
-      <div className="mapsBoxCont">maps</div>
+      <div className="mapsBoxCont">
+        <APIProvider apiKey={import.meta.env.VITE_MAPS_APIKEY}>
+          <Map
+            zoom={14}
+            center={position}
+            mapId={import.meta.env.VITE_MAPS_ID}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <Markers />
+          </Map>
+        </APIProvider>
+      </div>
     </section>
   );
 };
---
+++ src/component/maps/Markers.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { AdvancedMarker } from "@vis.gl/react-google-maps";
+
+const points = [
+  { key: "pont1", lat: 25.77427, lng: -80.19366 },
+  { key: "pont2", lat: 25.78448, lng: -80.19366 },
+  { key: "pont3", lat: 25.78348, lng: -80.20366 },
+];
+export const Markers = () => {
+  return (
+    <>
+      {points.map((point) => (
+        <AdvancedMarker position={point} key={point.key}>
+          <img
+            style={{
+              width: "25px",
+              backgroundColor: "black",
+              borderRadius: "50%",
+              padding: "0.4em",
+            }}
+            src="https://res.cloudinary.com/dksz8cxlh/image/upload/v1706311083/PROSIGMA/Sin-t%C3%ADtulo-1_za3hyt.png"
+            alt=""
+          />
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
